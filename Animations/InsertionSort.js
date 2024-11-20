17ab27d1f6a462194d8fb48d12676bf5722f9cf2
Luka-Rapaic <lukarapaic2014@gmail.com>
2024-11-20 16:12:21 +0100

    Fixed the insertion sort animation

--- Animations/InsertionSort.js
+++ Animations/InsertionSort.js
@@ -5,17 +5,19 @@ class InsertionSort {
     i = 0;
 
     elements = [
-        new Element(2, 10, 100, 100, 2),
-        new Element(102, 10, 100, 100, 5),
-        new Element(202, 10, 100, 100, 4),
-        new Element(302, 10, 100, 100, 1),
-        new Element(402, 10, 100, 100, 10),
-        new Element(502, 10, 100, 100, 25),
-        new Element(602, 10, 100, 100, 3),
-        new Element(702, 10, 100, 100, 7),
-        new Element(802, 10, 100, 100, 8),
-        new Element(902, 10, 100, 100, 18)
+        new Element(2, 120, 100, 100, 2),
+        new Element(102, 120, 100, 100, 5),
+        new Element(202, 120, 100, 100, 4),
+        new Element(302, 120, 100, 100, 1),
+        new Element(402, 120, 100, 100, 10),
+        new Element(502, 120, 100, 100, 25),
+        new Element(602, 120, 100, 100, 3),
+        new Element(702, 120, 100, 100, 7),
+        new Element(802, 120, 100, 100, 8),
+        new Element(902, 120, 100, 100, 18)
     ]
+
+    key = new Element(2, 10, 100, 100, '');
 
     generateEvents() {
         this.events = [];
@@ -27,34 +29,53 @@ class InsertionSort {
         for (let i = 1; i < arr.length; i++) {
             let key = arr[i];
             let j = i - 1;
-            this.events.push(['compare', key, j]);
+
+            this.events.push(['newKey', key, i]);
+
             for (; j >= 0 && arr[j] > key; j--) {
+                this.events.push(['compare', j]);
+
                 arr[j+1] = arr[j];
-                this.events.push(['switch', j, j+1]);
+                this.events.push(['move', j]);
             }
             arr[j+1] = key;
-            this.events.push(['insert', key, j+1]);
+            this.events.push(['insert', j+1]);
         }
         this.events.push(['complete']);
     }
 
     update() {
         let event = this.events[this.i];
-        if (event[0] === 'compare') {
+        if (event[0] === 'newKey') {
+            this.key.bgColor = '#cccccc';
             this.elements[event[2]].bgColor = '#cccccc';
-            this.draw();
-            this.elements[event[2]].bgColor = 'transparent';
 
-        } else if (event[0] === 'switch') {
-            let tmp = this.elements[event[1]].value;
-            this.elements[event[1]].value = this.elements[event[2]].value;
-            this.elements[event[2]].value = tmp;
+            this.key.value = event[1];
+            this.draw();
+
+            this.key.bgColor = 'transparent';
+            this.elements[event[2]].bgColor = 'transparent';
+        } else if (event[0] === 'compare') {
+            this.key.bgColor = '#cccccc';
+            this.elements[event[1]].bgColor = '#cccccc';
+
+            this.draw();
+
+            this.key.bgColor = 'transparent';
+            this.elements[event[1]].bgColor = '#D8F793';
+
+        } else if (event[0] === 'move') {
+            this.elements[event[1] + 1].bgColor = '#D8F793';
+            this.elements[event[1]].bgColor = 'transparent';
+
+            this.elements[event[1] + 1].value = this.elements[event[1]].value;
             this.draw();
 
         } else if (event[0] === 'insert') {
-            this.elements[event[2]].value = event[1];
+            this.elements[event[1]].bgColor = '#D8F793';
 
-            this.elements[event[2]].bgColor = '#cccccc';
+            this.elements[event[1]].value = this.key.value;
+
             this.draw();
 
         } else if (event[0] === 'complete') {
@@ -68,8 +89,9 @@ class InsertionSort {
 
     draw() {
         insertionSortCtx.fillStyle = "whitesmoke";
-        insertionSortCtx.fillRect(0, 0, 1004, 120);
+        insertionSortCtx.fillRect(0, 0, 1004, 230);
 
+        this.key.draw(insertionSortCtx);
         for (let element of this.elements) element.draw(insertionSortCtx);
     }
 }
@@ -79,6 +101,7 @@ let insertionSortCtx = insertionSortCanvas.getContext('2d', {alpha: false});
 
 let insertionSort = new InsertionSort();
 insertionSort.generateEvents();
+
 insertionSort.draw();
 
 function playInsertionSortAnimation() {
@@ -90,6 +113,12 @@ function playInsertionSortAnimation() {
         insertionSort.elements[i].value = insertionSort.startingState[i];
         insertionSort.elements[i].bgColor = 'transparent';
     }
+    if (insertionSort.elements.length >= 1) {
+        insertionSort.elements[0].bgColor = '#D8F793';
+    }
+    insertionSort.key.value = '';
+    insertionSort.key.bgColor = 'transparent';
+
     insertionSort.i = 0;
     insertionSort.draw();
 
